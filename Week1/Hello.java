07954be6ce6f623c2ce5f2d81554c6632a3ca484
sworup <phuyalsworup@gmail.com>
2024-04-14 12:25:23 +0545

    Task 2 completed

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -1,7 +1,8 @@
 /*  Write a program that prints Hello World! to the console. Save the program as Hello.java. */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
+        System.out.print("This is my first program\n I am on mod st");
     }
     
 }
